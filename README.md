0906ff8f373c6fcdcd9cd743eb9411464c410b69
b10715041 <b10715041@gmail.com>
2023-12-21 03:54:30 +0900

    Add the usage

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MineSweeper
+
+Minesweeper Game Usage:
+  Simply run the binary to start the game.
+  Use the WASD keys to move the cursor.
+  Press 'Enter' to reveal a cell.
+  Press 'F' to flag a cell as a mine.
+  The goal is to reveal all cells without mines.
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include "Game.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+    // Check if '--help' is one of the command line arguments
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg == "--help") {
+            std::cout << "Minesweeper Game Usage:\n";
+            std::cout << "  Simply run the binary to start the game.\n";
+            std::cout << "  Use the WASD keys to move the cursor.\n";
+            std::cout << "  Press 'Enter' to reveal a cell.\n";
+            std::cout << "  Press 'F' to flag a cell as a mine.\n";
+            std::cout << "  The goal is to reveal all cells without mines.\n";
+            return 0;
+        }
+    }
+
+    // Regular game initialization and execution
     int width = 10; // Board width
     int height = 10; // Board height
     int mines = 20; // Number of mines
